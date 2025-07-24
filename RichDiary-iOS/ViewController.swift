ca26fd74f12d14f9cbe274035b75c33cefc11803
OneTen19 <khy000426@naver.com>
2025-07-24 21:39:57 +0900

    Initial Commit

---
+++ RichDiary-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RichDiary-iOS
+//
+//  Created by OneTen on 7/24/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
