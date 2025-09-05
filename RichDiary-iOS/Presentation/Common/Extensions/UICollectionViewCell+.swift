5f41d2c50337be3ca4e93173a4d0e5c173746685
OneTen19 <khy000426@naver.com>
2025-09-05 21:10:28 +0900

    [Feat] #5 - Cell Extension

---
+++ RichDiary-iOS/Presentation/Common/Extensions/UICollectionViewCell+.swift
@@ -0,0 +1,17 @@
+//
+//  UICollectionViewCell+.swift
+//  RichDiary-iOS
+//
+//  Created by OneTen on 9/5/25.
+//
+
+import UIKit
+
+extension UICollectionViewCell {
+    
+    // Cell identifier
+    static var cellIdentifier : String {
+        return String(describing: self)
+    }
+    
+}
---
+++ RichDiary-iOS/Presentation/Common/Extensions/UITableViewCell+.swift
@@ -0,0 +1,17 @@
+//
+//  UITableViewCell+.swift
+//  RichDiary-iOS
+//
+//  Created by OneTen on 9/5/25.
+//
+
+import UIKit
+
+extension UITableViewCell {
+    
+    // Cell identifier
+    static var identifier : String {
+        return String(describing: self)
+    }
+    
+}
