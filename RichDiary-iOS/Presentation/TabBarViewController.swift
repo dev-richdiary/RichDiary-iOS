b782ffb912b4b3b025e94a1b8f630ed11532c4ff
OneTen19 <khy000426@naver.com>
2025-09-05 21:02:27 +0900

    [Feat] #5 - TabBarViewController

---
+++ RichDiary-iOS/Presentation/TabBarViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TabBarViewController.swift
+//  RichDiary-iOS
+//
+//  Created by OneTen on 7/24/25.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+}
+
