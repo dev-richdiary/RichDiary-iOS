4326f52c63da4b34ffa5043f715cd5d32bb03015
OneTen19 <khy000426@naver.com>
2025-09-07 01:54:29 +0900

    [Feat] #11 - HomeHeaderView 레이아웃 잡기

---
+++ RichDiary/Sources/Presentation/Home/View/HomeHeaderView.swift
@@ -0,0 +1,32 @@
+//
+//  HomeHeaderView.swift
+//  RichDiary
+//
+//  Created by OneTen on 9/7/25.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class HomeHeaderView: BaseUIView {
+    
+    //MARK: - UI Properties
+    
+    
+    
+    
+    
+    
+    //MARK: - Func
+    
+    override func setUI() {
+        self.backgroundColor = .red
+    }
+    
+}
+
+#Preview {
+    HomeHeaderView()
+}
---
+++ RichDiary/Sources/Presentation/Home/ViewController/HomeViewController.swift
@@ -0,0 +1,55 @@
+//
+//  HomeViewController.swift
+//  RichDiary-iOS
+//
+//  Created by OneTen on 9/5/25.
+//
+
+import UIKit
+
+import SnapKit
+
+final class HomeViewController: BaseUIViewController {
+    
+    // MARK: - UI Components
+    
+    private let scrollview = UIScrollView()
+    private let contentView = UIView()
+    private let headerView = HomeHeaderView()
+    
+    
+    //MARK: - Func
+    
+    override func setUI() {
+        self.view.addSubview(scrollview)
+        scrollview.addSubview(contentView)
+        contentView.addSubviews(headerView)
+    }
+    
+    override func setStyle() {
+        scrollview.do {
+            $0.showsVerticalScrollIndicator = false
+        }
+        
+    }
+    
+    override func setLayout() {
+        scrollview.snp.makeConstraints {
+            $0.edges.equalTo(view.safeAreaLayoutGuide.snp.edges)
+        }
+        
+        contentView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+            $0.width.equalToSuperview()
+            $0.bottom.equalToSuperview()
+            $0.height.equalTo(view.frame.height + 100)
+        }
+        
+        headerView.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.horizontalEdges.equalToSuperview()
+            $0.height.equalTo(52)
+            
+        }
+    }
+}
