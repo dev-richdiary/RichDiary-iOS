52146ac3665fa390e008959b9d6826e304aad0bf
OneTen19 <khy000426@naver.com>
2025-09-09 01:46:15 +0900

    [Feat] #13 - ExpenseDiaryModel

---
+++ RichDiary/Sources/Presentation/Diary/Components/DiaryTile.swift
@@ -0,0 +1,12 @@
+//
+//  DiaryTile.swift
+//  RichDiary
+//
+//  Created by OneTen on 9/9/25.
+//
+
+import Foundation
+
+final class DiaryTile: BaseUIView {
+    
+}
---
+++ RichDiary/Sources/Presentation/Diary/Model/InputDiaryModel.swift
@@ -0,0 +1,8 @@
+//
+//  InputDiaryModel.swift
+//  RichDiary
+//
+//  Created by OneTen on 9/9/25.
+//
+
+import Foundation
