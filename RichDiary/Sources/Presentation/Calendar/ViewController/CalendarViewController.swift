b49c1179982084aa239cb3332a19484f6e14ff27
OneTen19 <khy000426@naver.com>
2025-09-10 14:20:52 +0900

    [Style] #17 - CalendarHeaderView 배치

--- RichDiary/Sources/Presentation/Calendar/ViewController/CalendarViewController.swift
+++ RichDiary/Sources/Presentation/Calendar/ViewController/CalendarViewController.swift
@@ -7,6 +7,9 @@
 
 import UIKit
 
+import SnapKit
+import Then
+
 final class CalendarViewController: BaseUIViewController {
     
     // MARK: - UI Components
@@ -19,15 +22,36 @@ final class CalendarViewController: BaseUIViewController {
     //MARK: - Func
 
     override func setUI() {
-        <#code#>
+        self.view.addSubviews(headerView, scrollview)
+        scrollview.addSubview(contentView)
     }
     
     override func setStyle() {
-        <#code#>
+        scrollview.do {
+            $0.showsVerticalScrollIndicator = false
+            $0.backgroundColor = .white
+            $0.layer.cornerRadius = 40
+            $0.layer.maskedCorners = CACornerMask(arrayLiteral: .layerMinXMinYCorner, .layerMaxXMinYCorner)
+        }
     }
     
     override func setLayout() {
-        <#code#>
+        headerView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(-20)
+            $0.horizontalEdges.equalToSuperview()
+            $0.height.equalTo(52)
+        }
+        
+        scrollview.snp.makeConstraints {
+            $0.top.equalTo(headerView.snp.bottom).offset(25)
+            $0.horizontalEdges.equalTo(view.safeAreaLayoutGuide)
+            $0.bottom.equalTo(view.safeAreaLayoutGuide)
+        }
+        
+        contentView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+            $0.width.equalToSuperview()
+        }
     }
     
 }
--- RichDiary/Sources/Presentation/Home/ViewController/HomeViewController.swift
+++ RichDiary/Sources/Presentation/Home/ViewController/HomeViewController.swift
@@ -63,7 +63,7 @@ final class HomeViewController: BaseUIViewController {
     
     override func setLayout() {
         headerView.snp.makeConstraints {
-            $0.top.equalTo(view.safeAreaLayoutGuide).offset(-30)
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(-20)
             $0.horizontalEdges.equalToSuperview()
             $0.height.equalTo(52)
         }
