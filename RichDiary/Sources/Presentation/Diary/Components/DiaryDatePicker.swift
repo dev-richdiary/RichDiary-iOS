095c0fd00afafd3aedfd5324ed883dc9fa2fef2d
OneTen19 <khy000426@naver.com>
2025-09-12 14:20:41 +0900

    [Feat] #21 - DiartDatePicker 컴포넌트 분리

---
+++ RichDiary/Sources/Presentation/Diary/Components/DiaryDatePicker.swift
@@ -0,0 +1,121 @@
+//
+//  DiaryDatePicker.swift
+//  RichDiary
+//
+//  Created by OneTen on 9/12/25.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class DiaryDatePicker: BaseUIView {
+    
+    // MARK: - Properties
+
+    public var date: Date = Date() {
+        didSet {
+            // 날짜가 변경되면 텍스트 필드의 내용도 업데이트
+            dateTextField.text = formatDate(date: date)
+        }
+    }
+
+    
+    // MARK: - UI Components
+
+    private let dateLabel = UILabel()
+    private let dateTextField = UITextField()
+    private let datePicker = UIDatePicker()
+    
+    
+    // MARK: - init
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupDatePicker()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    
+    // MARK: - override Func
+
+    override func setUI() {
+        self.addSubviews(dateLabel, dateTextField)
+    }
+    
+    override func setStyle() {
+        dateLabel.do {
+            $0.attributedText = .richStyle("날짜", style: .custom(fontWeight: .bold, size: 16))
+        }
+        
+        dateTextField.do {
+            $0.borderStyle = .roundedRect
+            $0.textAlignment = .center
+            $0.delegate = self
+            $0.tintColor = .clear
+        }
+    }
+    
+    override func setLayout() {
+        dateLabel.snp.makeConstraints {
+            $0.top.leading.trailing.equalToSuperview()
+        }
+        
+        dateTextField.snp.makeConstraints {
+            $0.top.equalTo(dateLabel.snp.bottom).offset(10)
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(44)
+            $0.bottom.equalToSuperview()
+        }
+    }
+}
+
+
+// MARK: - Private Func
+
+extension DiaryDatePicker {
+    private func setupDatePicker() {
+        datePicker.do {
+            $0.datePickerMode = .date
+            $0.preferredDatePickerStyle = .wheels
+            $0.locale = Locale(identifier: "ko_KR")
+        }
+        
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        let doneButton = UIBarButtonItem(title: "완료", style: .done, target: self, action: #selector(didTapDoneOnDatePicker))
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        toolbar.setItems([flexSpace, doneButton], animated: true)
+        
+        dateTextField.do {
+            $0.inputView = datePicker
+            $0.inputAccessoryView = toolbar
+            $0.text = formatDate(date: self.date)
+        }
+    }
+    
+    private func formatDate(date: Date) -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy년 MM월 dd일"
+        formatter.locale = Locale(identifier: "ko_KR")
+        return formatter.string(from: date)
+    }
+    
+    @objc private func didTapDoneOnDatePicker() {
+        self.date = datePicker.date
+        self.endEditing(true)
+    }
+}
+
+
+// MARK: - UITextFieldDelegate
+
+extension DiaryDatePicker: UITextFieldDelegate {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        return false // 텍스트 필드에 직접적인 입력 방지
+    }
+}
